664e17dbcdf4f5d5e2a789d7a18ea7ba671dec25
Sasha Oslaj <sasa.oslaj@student.fmf.uni-lj.si>
2020-03-28 11:23:19 +0100

    Sprememba v datoteki za predstavitev podatkov.

--- predstavitev_podatkov.py
+++ predstavitev_podatkov.py
@@ -1,4 +1,4 @@
-
+import random
 ########################################################################################################################
 ########################################################################################################################
 
@@ -28,14 +28,14 @@ class Tekmovalec:
         return 'Tekmovalec({0},{1},{2})'.format(self.ime, self.nastopi, self.rojen)
 
     def natancna_predstavitev_tekmovalca(self):
+        seznam=[]
         for nastop in self.nastopi:
+            if self.rojen == '':
+                seznam.append((self.ime, self.rojen, self.drzava, nastop[0], nastop[1], nastop[2], nastop[4].replace('"',''), 'Ni podatka'))
             starost = int(nastop[0][-4:]) - int(self.rojen[-4:])
-            print(
-                " ime        | {0} \n rojen/a    | {1} v {2} \n oi         | {3} \n disciplina | {4} \n mesto      | {5} \n rezultat   | {6} \n starost    | {7} \n".format(
-                    self.ime, self.rojen, self.drzava, nastop[0], nastop[1], nastop[2], nastop[4].replace('"',''), str(starost)
-                )
-            )
-
+            seznam.append((self.ime, self.rojen, self.drzava, nastop[0], nastop[1], nastop[2], nastop[4].replace('"',''), str(starost)))
+        return seznam
+            
     def podatki_po_disciplinah(self):
         slovar={}
         for nastop in self.nastopi:
@@ -44,6 +44,70 @@ class Tekmovalec:
             else:
                 slovar[nastop[1]] = [nastop]
         return slovar
+
+    def predstavitev_tekmovalca_pri_disciplini(self, disciplina=None):
+        seznam=[]
+        if disciplina==None:          
+            disciplina=random.choice(self.discipline)
+            for i in self.podatki_po_disciplinah()[disciplina]:
+                if self.rojen == '':
+                    seznam.append((self.ime, self.rojen, self.drzava, i[0], i[1], i[2], i[4].replace('"',''), 'Ni podatka'))
+                starost = int(i[0][-4:]) - int(self.rojen[-4:])
+                seznam.append((self.ime, self.rojen, self.drzava, i[0], i[1], i[2], i[4].replace('"',''), str(starost)))
+        if disciplina not in self.podatki_po_disciplinah():
+            seznam.append(('Tekmovalec ni nastopil v {} disciplini'.format(disciplina)))
+        else:
+            for i in self.podatki_po_disciplinah()[disciplina]:
+                if self.rojen == '':
+                    seznam.append((self.ime, self.rojen, self.drzava, i[0], i[1], i[2], i[4].replace('"',''), 'Ni podatka'))
+                starost = int(i[0][-4:]) - int(self.rojen[-4:])
+                seznam.append((self.ime, self.rojen, self.drzava, i[0], i[1], i[2], i[4].replace('"',''), str(starost)))
+        return seznam
+    
+    def iz_katere_drzave(self):
+        return self.nastop[0][3]
+
+class Tekmovalci:
+
+    def __init__(self, vsi_rezultati):
+        drzave=set()
+        discipline=set()
+        oi=set()
+        for tekmovalec in vsi_rezultati:
+            for nastop in vsi_rezultati[tekmovalec][:-1]:
+                drzave.add(nastop[3])
+                discipline.add(nastop[1])
+                oi.add(nastop[0])
+        self.vse_drzave=drzave
+        self.vse_discipline=discipline
+        self.oi=oi
+        self.vsi_rezultati=vsi_rezultati
+        self.sez_tekmovalcev=list(vsi_rezultati.keys())
+
+    def __str__(self):
+        return "Ta razred predstavlja rezultate {} olimpijskih iger v {} disciplinah iz {} džav.".format(
+            len(self.oi), len(self.vse_discipline), len(self.vse_drzave)
+            )
+    def __repr__(self):
+        return "Tekmovalci({})".format(self.vsi_tekmovalci)
+
+    def izpisi_tekmovalca(self, ime=None):
+        if ime == None:
+            ime = random.choice(self.sez_tekmovalcev)
+            return Tekmovalec(ime, podatki_tekmovalcev[ime][:-1], podatki_tekmovalcev[ime][-1])
+        else:
+            return Tekmovalec(ime, podatki_tekmovalcev[ime][:-1], podatki_tekmovalcev[ime][-1])
+
+    def natancno_izpisi_tekmovalca(self, ime=None):
+        if ime == None:
+            ime = random.choice(self.sez_tekmovalcev)
+            return Tekmovalec(ime, podatki_tekmovalcev[ime][:-1], podatki_tekmovalcev[ime][-1]).natancna_predstavitev_tekmovalca()
+        else:
+            return Tekmovalec(ime, podatki_tekmovalcev[ime][:-1], podatki_tekmovalcev[ime][-1]).natancna_predstavitev_tekmovalca()
+            
+
+            
+            
 
 
 ########################################################################################################################
@@ -82,12 +146,14 @@ with open('roj_dan_tekmovalcev.csv', 'r', encoding="utf-8") as dat:
 
 # Podatki, ki jih uporabimo
 
+#print(podatki_tekmovalcev)
 #for i in sorted(podatki_tekmovalcev)[1:]:
-#    print(Tekmovalec(i, podatki_tekmovalcev[i][:-1], podatki_tekmovalcev[i][-1]))
+#    print(i,podatki_tekmovalcev[i])
 
-Tekmovalec(
-    "Mohamed Farah", podatki_tekmovalcev["Mohamed Farah"][:-1], podatki_tekmovalcev["Mohamed Farah"][-1]
-).natancna_predstavitev_tekmovalca()
-
+#Tekmovalec(
+#    "Mohamed Farah", podatki_tekmovalcev["Mohamed Farah"][:-1], podatki_tekmovalcev["Mohamed Farah"][-1]
+#).predstavitev_tekmovalca_pri_disciplini("5000m men")
+print(Tekmovalci(podatki_tekmovalcev).natancno_izpisi_tekmovalca())
+#print(podatki_tekmovalcev["Mohamed Farah"])
 
 
